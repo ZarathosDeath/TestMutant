a157c705e45ba92f7172be52dc09320b14b6534f
ZarathosDeath <nando_rhapsody@hotmail.com>
2020-10-05 17:27:11 -0300

    feat: ensure DownloadUsersController returns 200 on success

--- src/controllers/test/mockDownloadUsers.ts
+++ src/controllers/test/mockDownloadUsers.ts
@@ -1,8 +1,19 @@
 import { DownloadUsers } from "@/models/protocols"
 import { User } from "@/models/User"
+import * as faker from 'faker'
+
+export const mockUsersModel = () => {
+  return {
+    id: 1,
+    name: faker.name.findName(),
+    username: faker.random.word(),
+    phone: faker.phone.phoneNumber(),
+    website: faker.internet.url()
+  }
+}
 
 export class DownloadUsersSpy implements DownloadUsers {
-  usersModel: []
+  usersModel: User[]
 
   async download (): Promise<User[]> {
     return this.usersModel
