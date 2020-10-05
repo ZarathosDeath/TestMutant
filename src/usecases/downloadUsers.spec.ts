16ce4e517f5e6a4ed282bb57c64f9ab3b2214a7c
ZarathosDeath <nando_rhapsody@hotmail.com>
2020-10-05 15:06:33 -0300

    test: ensure downloadUsers returns a list of users on success

---
+++ src/usecases/downloadUsers.spec.ts
@@ -0,0 +1,37 @@
+const axios = require('axios')
+const faker =  require('faker')
+const { DbDownloadUsers } = require('./downloadUsers')
+const mocked = require('ts-jest')
+
+jest.mock('axios')
+
+describe('DownloadUsers usecase', () => {
+  const usersModelMock = [
+    {
+      id: 1,
+      name: faker.name.findName(),
+      username: faker.random.word(),
+      phone: faker.phone.phoneNumber(),
+      website: faker.internet.url()
+    },
+    {
+      id: 1,
+      name: faker.name.findName(),
+      username: faker.random.word(),
+      phone: faker.phone.phoneNumber(),
+      website: faker.internet.url()
+    }
+  ]
+
+  test('should return a list of Users on success', async () => {
+    axios.get.mockResolvedValue({
+      data: usersModelMock
+    })
+
+    const downloadUsers = new DbDownloadUsers()
+
+    const res = await downloadUsers.download()
+    expect(axios.get).toHaveBeenCalled()
+    expect(res.data).toEqual(usersModelMock)
+  })
+})
